791237a5d8a58509234a045d10442579f8670276
kishore951 <kishoresakthivel951@gmail.com>
2022-10-07 21:08:15 +0530

    second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdddonarComponent } from './adddonar/adddonar.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SearchdonarComponent } from './searchdonar/searchdonar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdddonarComponent,
-    NavbarComponent
+    NavbarComponent,
+    SearchdonarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/searchdonar/searchdonar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searchdonar',
+  templateUrl: './searchdonar.component.html',
+  styleUrls: ['./searchdonar.component.css']
+})
+export class SearchdonarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
