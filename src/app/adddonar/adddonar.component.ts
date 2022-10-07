cd8210ec99f58014540906fbf5a0c94700c9a4c9
kishore951 <kishoresakthivel951@gmail.com>
2022-10-07 20:54:18 +0530

    koo

---
+++ src/app/adddonar/adddonar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adddonar',
+  templateUrl: './adddonar.component.html',
+  styleUrls: ['./adddonar.component.css']
+})
+export class AdddonarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
